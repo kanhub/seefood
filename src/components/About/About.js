f36dee9dd42854426c2961b55a0b0cd127d1baa8
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-11-22 20:10:55 -0500

    updated about page with test credentials

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,47 @@
 import React from "react";
-import { Typography, Card } from "antd";
+import { Typography, Card, List } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 const { Text, Title } = Typography;
 
 const About = () => {
+  const credentials = [
+    {
+      email: "john@gmail.com",
+      password: "test",
+    },
+  ];
+
   return (
     <div id="about-container">
       <Card className="login" title={<Title level={3}>About</Title>}>
         <Text>This is demo app built for fun!</Text>
+        <br />
+        <br />
+        <Text>
+          Test credentials:
+          <br />
+          <br />
+          <List
+            itemLayout="horizontal"
+            dataSource={credentials}
+            renderItem={(item) => (
+              <List.Item.Meta
+                description={
+                  <>
+                    <Text>
+                      <UserOutlined /> {item.email}
+                    </Text>
+                    <br />
+                    <Text>
+                      <LockOutlined /> {item.password}
+                    </Text>
+                  </>
+                }
+              />
+            )}
+          />
+        </Text>
       </Card>
     </div>
   );
